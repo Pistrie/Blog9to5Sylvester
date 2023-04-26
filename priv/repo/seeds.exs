bc8000dbe6ae7aa2993468fb8b890cd554cee47b
Sylvester Roos <sylvester@thejoyofcoding.nl>
2023-04-26 13:17:01 +0200

    Created seed data

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,26 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias BlogNineToFiveSylvester.Blog.Post
+alias BlogNineToFiveSylvester.Blog.Comment
+alias BlogNineToFiveSylvester.Repo
+
+{:ok, _post} =
+  %Post{
+    author: "José Valim",
+    title: "Blog post #1",
+    text: "This is a very interesting blog post",
+    comments: [
+      %Comment{
+        text: "Good post",
+        author: "John Doe"
+      },
+      %Comment{
+        text: "Love it!",
+        author: "Ada Lovelace"
+      }
+    ]
+  }
+  |> Repo.insert()
+  |> IO.inspect()
