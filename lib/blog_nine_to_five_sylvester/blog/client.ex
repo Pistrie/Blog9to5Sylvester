7a96bddb090505b7c09e2315c5d225cd8de2be26
Sylvester Roos <sylvester@thejoyofcoding.nl>
2023-05-31 13:46:07 +0200

    created api client for getting true but useless facts

---
+++ lib/blog_nine_to_five_sylvester/blog/client.ex
@@ -0,0 +1,22 @@
+defmodule BlogNineToFiveSylvester.Blog.Client do
+  use Tesla, only: [:get], docs: false
+
+  plug Tesla.Middleware.BaseUrl, "https://uselessfacts.jsph.pl/api/v2/"
+  plug Tesla.Middleware.JSON
+
+  def fetch_random_fact do
+    "/facts/random"
+    |> fact_in_english()
+    |> get()
+    |> extract_fact_field()
+  end
+
+  defp fact_in_english(url) do
+    url <> "?language=en"
+  end
+
+  defp extract_fact_field(http_response) do
+    {:ok, %{body: %{"text" => text}}} = http_response
+    text
+  end
+end
