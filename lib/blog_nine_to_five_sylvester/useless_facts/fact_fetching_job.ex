24301d0acd4937d3aa0d3de5b67a836c1e9a4de2
Sylvester Roos <sylvester@thejoyofcoding.nl>
2023-06-01 10:33:47 +0200

    made oban job more readable

--- lib/blog_nine_to_five_sylvester/useless_facts/fact_fetching_job.ex
+++ lib/blog_nine_to_five_sylvester/useless_facts/fact_fetching_job.ex
@@ -5,16 +5,16 @@ defmodule BlogNineToFiveSylvester.UselessFacts.FactFetchingJob do
   alias BlogNineToFiveSylvester.UselessFacts.Client
 
   @impl Oban.Worker
-  def perform(%Oban.Job{}) do
-    case Client.fetch_random_fact() do
-      {:ok, fact} ->
-        Logger.info(fact)
-        :ok
+  def perform(%Oban.Job{}), do: Client.fetch_random_fact() |> do_fetch()
 
-      {:error, error} ->
-        error_msg = "Failed to get fact: #{inspect(error)}"
-        Logger.error(error_msg)
-        {:error, error_msg}
-    end
+  defp do_fetch({:ok, fact}) do
+    Logger.info(fact)
+    :ok
+  end
+
+  defp do_fetch({:error, reason}) do
+    error_msg = "Failed to get fact: #{inspect(reason)}"
+    Logger.error(error_msg)
+    {:error, error_msg}
   end
 end
